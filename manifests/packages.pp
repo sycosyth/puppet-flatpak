f9cbdbeea225dcf893f1a30b1dec5b987aca2dba
Matt Mercier <mmercier@cbnco.com>
2021-11-01 22:00:58 -0400

    manage flatpak messages

---
+++ manifests/packages.pp
@@ -0,0 +1,13 @@
+# Install flatpak packages
+class flatpak::packages() {
+
+    $package_defaults = {
+        ensure  => 'present',   # ensure => 'present' is the default for ensure_packages.
+    }
+
+    # Install packagages that are marked for installation in hiera
+    $flatpak_packages_hash = lookup('flatpak::packages', Hash, $::cbn_deep_merge, {})
+    info ("${::facts['fqdn']}: flatpak_packages:\n${flatpak_packages_hash.to_yaml}")
+    create_resources('flatpak', $system_packages_hash)
+
+}
